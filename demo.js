422070c2953789d34b7dd40e3cdae3830269fd0d
Taz Larson <tazlarson1@gmail.com>
2024-01-24 17:25:00 -0800

    demo

--- demo.js
+++ demo.js
@@ -2,9 +2,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     console.log("Hellow World!")
 
     const body = document.querySelector("body");
+    const content = document.getElementById("content");
     const btn = document.getElementById("clickme");
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", async () => {
         body.classList.toggle("dark");
-        console.log("click");
+        
+        try {
+            let randomWord = document.createElement("p");
+            const response = await fetch("https://random-word-api.herokuapp.com/word")
+            const data = await response.json();
+            randomWord.textContent = data[0];
+            content.appendChild(randomWord);
+
+        }catch (err) {
+            console.log(err);
+        }
+        
+        
     })
 });
